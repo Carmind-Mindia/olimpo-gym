5d8c7fd3d0e5705ac5043c198328e91910eb87a7
Alexis <alexisfonzos_@hotmail.com>
2024-02-04 19:14:32 -0300

    Update app.listen port to use environment variable

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = new Elysia()
 routeMembresia(app);
 routeSubscriptor(app);
 
-app.listen(3000);
+app.listen(Bun.env.PORT || 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
